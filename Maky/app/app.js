1c5d67939e28c708a5cdce372e86c15e6bf69dbf
adizdar1@hotmail.com <dizdar.ahmed@gmail.com>
2014-08-23 22:50:24 +0200

    --Master Commit--

---
+++ Maky/app/app.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="../Scripts/ionic.bundle.min.js" />
+
+angular.module('maky', ['ionic'])
+
+.config(function ($urlRouterProvider, $stateProvider) {
+
+    $stateProvider
+    //works with ion-nav-view
+    //.state('home', {
+    //    url: '/',
+    //    templateUrl: "app/View/home/page.html"
+
+    //})
+
+    $stateProvider
+    .state('home', {
+        url: '/',
+
+        views: {
+            'default': {
+                templateUrl: "app/View/home/page.html"
+            },
+            'header': {
+                templateUrl: "app/View/header/headerHome.html"
+            }
+        }
+    })
+
+
+
+    $urlRouterProvider.otherwise('/');
+
+});
+
