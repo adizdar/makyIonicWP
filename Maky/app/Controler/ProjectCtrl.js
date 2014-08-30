872a01d0b8b92290d6bd3a9624384dbcfbe52093
adizdar1@hotmail.com <dizdar.ahmed@gmail.com>
2014-08-30 14:50:07 +0200

    Create maky card modal doen and popup title changed

--- Maky/app/Controler/ProjectCtrl.js
+++ Maky/app/Controler/ProjectCtrl.js
@@ -1,17 +1,45 @@
 ﻿angular.module('maky.controllers', [])
 
-.controller('ProjectCtrl', function ($scope, $ionicPopup) {
+.controller('ProjectCtrl', function ($scope, $ionicPopup, $ionicModal) {
 
     //show popup on page initialization
     $scope.init = function () {
         $ionicPopup.prompt({
             title: "Enter project name",
-            inputType: 'text'
+            inputType: 'text',
+            buttons: [{ text: 'OK', type: 'button-light' }]
+
         }).then(function (title) {
             //add page title
             $scope.projectTitle = title;
         });
     };
+
+    //create scrum card modal
+        $ionicModal.fromTemplateUrl('app/Template/popup-scrumCard.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.scrumCardModal = modal;
+        });
+        $scope.openModal = function () {
+            $scope.modal.show();
+        };
+        $scope.closeModal = function () {
+            $scope.modal.hide();
+        };
+        //Cleanup the modal when we're done with it!
+        $scope.$on('$destroy', function () {
+            $scope.modal.remove();
+        });
+        // Execute action on hide modal
+        $scope.$on('modal.hidden', function () {
+            // Execute action
+        });
+        // Execute action on remove modal
+        $scope.$on('modal.removed', function () {
+            // Execute action
+        });
+
+
 });
-
-
